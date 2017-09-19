4d48430954071977bcd3473a810e4000b2b649f0
Srivatsa Marichi <ecanarys1@outlook.com>
2017-09-19 14:18:28 +0530

    adding script

---
+++ VSCode.ps1
@@ -0,0 +1,12 @@
+﻿#Set Execution Policy
+Set-ExecutionPolicy Unrestricted -Force
+
+#Create Folder
+cd \
+mkdir Downloads
+
+#Download Visual Studio Code
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?Linkid=852157" -OutFile "C:\Downloads\vscode.exe"
+
+#Silent Installation of Visual Studio Code
+C:\Downloads\vscode.exe /VERYSILENT /MERGETASKS=!runcode
